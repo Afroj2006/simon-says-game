b1c06cb9e3e4861deb1614df88849e893ec5c65d
Afroj Ali <afrojali5146@gmail.com>
2026-02-14 00:58:24 +0530

    change

--- script.js
+++ script.js
@@ -1,62 +1,112 @@
-let buttonColors = ["red", "yellow", "green", "blue"];
-let gamePattern = [];
-let userPattern = [];
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "yellow", "green", "purple"];
+
 let started = false;
 let level = 0;
 
-document.addEventListener("keydown", startGame);
-document.addEventListener("touchstart", startGame);
+let h2 = document.querySelector("h2");
 
-function startGame() {
-  if (!started) {
-    level = 0;
-    gamePattern = [];
-    started = true;
-    nextSequence();
-  }
-}
-
-function nextSequence() {
-  userPattern = [];
-  level++;
-  document.getElementById("level-title").innerText = "Level " + level;
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomColor = buttonColors[randomNumber];
-  gamePattern.push(randomColor);
-  flashButton(randomColor);
-}
-
-function flashButton(color) {
-  let button = document.getElementById(color);
-  button.classList.add("pressed");
-  setTimeout(function () {
-    button.classList.remove("pressed");
-  }, 200);
-}
-
-document.querySelectorAll(".btn").forEach(function (button) {
-  button.addEventListener("click", function () {
-    if (!started) return;
-    let userColor = this.id;
-    userPattern.push(userColor);
-    flashButton(userColor);
-    checkAnswer(userPattern.length - 1);
-  });
+document.addEventListener("click", function () {
+    if (started === false) {
+        console.log("Game Started");
+        started = true;
+        levelUp();
+    }
 });
 
-function checkAnswer(currentIndex) {
-  if (userPattern[currentIndex] === gamePattern[currentIndex]) {
-    if (userPattern.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSequence();
-      }, 800);
+document.addEventListener("touchstart", function () {
+    if (started === false) {
+        console.log("Game Started");
+        started = true;
+        levelUp();
     }
-  } else {
-    document.body.classList.add("game-over");
-    document.getElementById("level-title").innerText = "Game Over, Tap To Restart";
-    setTimeout(function () {
-      document.body.classList.remove("game-over");
-    }, 200);
+});
+
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(() => {
+        btn.classList.remove("flash");
+    }, 500);
+}
+
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(() => {
+        btn.classList.remove("userflash");
+    }, 500);
+}
+
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * btns.length);
+    let randColor = btns[randIdx];
+    gameSeq.push(randColor);
+
+    console.log("Game Sequence:", gameSeq);
+
+    let delay = 0;
+    for (let color of gameSeq) {
+        let btn = document.querySelector(`.${color}`);
+        setTimeout(() => {
+            gameFlash(btn);
+        }, delay);
+        delay += 600;
+    }
+}
+
+
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `
+            Game Over! <br>
+            Your Score: <b>${level - 1}</b> <br>
+            Tap anywhere to restart
+        `;
+        console.log("Game Over! Final Score:", level - 1);
+        reset();
+    }
+}
+
+
+function btnPress() {
+    if (!started) return;
+
+    let btn = this;
+    userFlash(btn);
+
+    let userColor = btn.getAttribute("id");
+    console.log("User clicked:", userColor);
+
+    userSeq.push(userColor);
+    console.log("User Sequence:", userSeq);
+
+    checkAns(userSeq.length - 1);
+}
+
+
+function reset() {
     started = false;
-  }
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+
+let allBtns = document.querySelectorAll(".btn");
+
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+    btn.addEventListener("touchstart", btnPress);
 }
